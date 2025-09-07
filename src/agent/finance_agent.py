ec85304db7ccbe1d5e896b3e6de98d6664e2ae56
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-09-07 19:29:27 -0300

    refact: update agent to use langgraph workflow instead of orchestrating nodes

--- src/agent/finance_agent.py
+++ src/agent/finance_agent.py
@@ -1,37 +1,34 @@
 import os
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 
 from src.agent.state import AgentState
-from src.nodes.finance_nodes import (
-    fetch_ticker_node,
-    fetch_recommendations_node,
-    fetch_fundamentals_node,
-    fetch_news_node,
-)
-from src.nodes.summarizer_node import summarizer_node
+from src.graph.workflow import graph
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 
 class FinanceAgent:
-    def __init__(self, ticker: str):
-        self.ticker = ticker
+    def __init__(self, user_message: HumanMessage):
+        self.user_message = user_message
         self.llm = ChatOpenAI(
             temperature=0, model="gpt-4o-mini", api_key=OPENAI_API_KEY
         )
 
     def run(self) -> AgentState:
         state: AgentState = {
-            "ticker": self.ticker,
+            "user_message": self.user_message,
+            "llm": self.llm,
+            "ticker": "",
+            "requested_info": [],
             "ticker_info": {},
             "summary": None,
         }
-        state = fetch_ticker_node(state, self.ticker)
-        state = fetch_recommendations_node(state, self.ticker)
-        state = fetch_fundamentals_node(state, self.ticker)
-        state = fetch_news_node(state, self.ticker)
-        state = summarizer_node(state, self.llm)
-        return state
+        for event in graph.stream(state):
+            print(f"Node: {event}")
+
+        final_state = graph.invoke(state)
+        return final_state
 
     def get_summary(self) -> AgentState:
         state = self.run()
