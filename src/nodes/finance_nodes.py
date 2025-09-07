f76cc893358b588dc36c7d0af2b22f0ee30f599d
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-09-07 16:54:44 -0300

    feature: add new nodes

--- src/nodes/finance_nodes.py
+++ src/nodes/finance_nodes.py
@@ -1,4 +1,9 @@
-from src.tools.finance_tools import get_current_stock_price
+from src.tools.finance_tools import (
+    get_current_stock_price,
+    get_analyst_recommendations,
+    get_stock_fundamentals,
+    get_company_news,
+)
 from src.agent.state import AgentState
 
 
@@ -12,5 +17,55 @@ def fetch_ticker_node(state: AgentState, ticker: str) -> AgentState:
     Returns:
         dict | str: A dictionary containing key stock fundamentals or an error message.
     """
-    stock_info = get_current_stock_price(ticker)
-    return AgentState(**{**state, "ticker": ticker, "ticker_info": stock_info})
+    stock_price_info = get_current_stock_price(ticker)
+    ticker_info = state.get("ticker_info", {})
+    ticker_info["current_price"] = stock_price_info
+    return AgentState(**{**state, "ticker": ticker, "ticker_info": ticker_info})
+
+
+def fetch_recommendations_node(state: AgentState, ticker: str) -> AgentState:
+    """
+    Fetch analyst recommendations for a stock ticker.
+
+    Args:
+        ticker (str): The ticker symbol of the stock.
+
+    Returns:
+        dict | str: A JSON string containing analyst recommendations or an error message.
+    """
+    stock_recommendations = get_analyst_recommendations(ticker)
+    ticker_info = state.get("ticker_info", {})
+    ticker_info["recommendations"] = stock_recommendations
+    return AgentState(**{**state, "ticker": ticker, "ticker_info": ticker_info})
+
+
+def fetch_fundamentals_node(state: AgentState, ticker: str) -> AgentState:
+    """
+    Fetch stock fundamentals for a stock ticker.
+
+    Args:
+        ticker (str): The ticker symbol of the stock.
+
+    Returns:
+        dict | str: A JSON string containing key stock fundamentals or an error message.
+    """
+    stock_fundamentals = get_stock_fundamentals(ticker)
+    ticker_info = state.get("ticker_info", {})
+    ticker_info["fundamentals"] = stock_fundamentals
+    return AgentState(**{**state, "ticker": ticker, "ticker_info": ticker_info})
+
+
+def fetch_news_node(state: AgentState, ticker: str) -> AgentState:
+    """
+    Fetch recent news articles for a stock ticker.
+
+    Args:
+        ticker (str): The ticker symbol of the stock.
+
+    Returns:
+        dict | str: A JSON string containing recent news articles or an error message.
+    """
+    stock_news = get_company_news(ticker)
+    ticker_info = state.get("ticker_info", {})
+    ticker_info["news"] = stock_news
+    return AgentState(**{**state, "ticker": ticker, "ticker_info": stock_news})
