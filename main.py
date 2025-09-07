fc886bdc9d691d901abee004735822d1a7efccb3
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-09-07 19:31:52 -0300

    refact: update agent input in main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from langchain_core.messages import HumanMessage
 from src.agent.finance_agent import FinanceAgent
 
 
 def main():
-    agent = FinanceAgent("AAPL")
+    user_message = HumanMessage(
+        content="What is the current available stock price and recommendations for AAPL?"
+    )
+    agent = FinanceAgent(user_message)
     result = agent.run()
     print(result["summary"])
 
