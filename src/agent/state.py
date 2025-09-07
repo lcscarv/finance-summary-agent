34616d594b8a2bc3e1490872cbd4369cd5610916
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-09-07 19:29:01 -0300

    feature: add llm as state variable

--- src/agent/state.py
+++ src/agent/state.py
@@ -1,9 +1,11 @@
 from typing import Optional, TypedDict
 from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
 
 
 class AgentState(TypedDict):
-    user_message: Optional[HumanMessage]
+    user_message: HumanMessage
+    llm: ChatOpenAI
     ticker: Optional[str]
     requested_info: Optional[list[str]]
     ticker_info: Optional[dict]
