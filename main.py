7acea35437e3927444d120a40dfa05824e4a3635
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-09-13 12:35:30 -0300

    refact: update main to consider general response output

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from src.agent.finance_agent import FinanceAgent
 
 
 def main():
-    user_message = HumanMessage(
-        content="What is the current available stock price and recommendations for AAPL?"
-    )
+    user_message = HumanMessage(content="What is the capital of Brazil?")
     agent = FinanceAgent(user_message)
     result = agent.run()
+    if result["general_response"]:
+        print(result["general_response"])
     print(result["summary"])
 
 
