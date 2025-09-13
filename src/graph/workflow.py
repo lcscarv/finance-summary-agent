d1224cd0401a6cc4d12ae3e6fb9dbc2e537c2cb5
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-09-13 12:34:59 -0300

    refact: update workflow to consider general response

--- src/graph/workflow.py
+++ src/graph/workflow.py
@@ -7,7 +7,14 @@ from src.nodes.finance_nodes import (
     fetch_news_node,
 )
 from src.nodes.intent_node import intent_node
+from src.nodes.general_chat_node import general_chat_node
 from src.nodes.summarizer_node import summarizer_node
+
+
+def intent_router(state: AgentState) -> str:
+    if "general_chat" in state["requested_info"]:
+        return "general"
+    return "finance"
 
 
 workflow = StateGraph(state_schema=AgentState)
@@ -18,13 +25,17 @@ workflow.add_node("fetch_recommendations", fetch_recommendations_node)
 workflow.add_node("fetch_fundamentals", fetch_fundamentals_node)
 workflow.add_node("fetch_news", fetch_news_node)
 workflow.add_node("summarize", summarizer_node)
+workflow.add_node("general_chat", general_chat_node)
 
 workflow.add_edge(START, "intent")
-workflow.add_edge("intent", "fetch_ticker")
+workflow.add_conditional_edges(
+    "intent", intent_router, {"finance": "fetch_ticker", "general": "general_chat"}
+)
 workflow.add_edge("fetch_ticker", "fetch_recommendations")
 workflow.add_edge("fetch_recommendations", "fetch_fundamentals")
 workflow.add_edge("fetch_fundamentals", "fetch_news")
 workflow.add_edge("fetch_news", "summarize")
 workflow.add_edge("summarize", END)
 
+workflow.add_edge("general_chat", END)
 graph = workflow.compile()
